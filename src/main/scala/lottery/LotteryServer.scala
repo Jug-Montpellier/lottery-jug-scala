e2185709c5e3d060512bd142542adaa2eb73a07f
Olivier NOUGUIER <olivier.nouguier@hawk-platform.io>
2016-10-19 15:27:34 +0200

    Remove email from answer.
    Handle resources not found.

--- src/main/scala/lottery/LotteryServer.scala
+++ src/main/scala/lottery/LotteryServer.scala
@@ -1,20 +1,23 @@
 package lottery
 
 import akka.actor.{ActorSystem, Props}
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.common.EntityStreamingSupport
-import akka.http.scaladsl.marshalling.ToResponseMarshaller
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.StandardRoute
-import akka.stream.{ActorMaterializer, SourceShape}
-import akka.stream.scaladsl.{FileIO, Source, StreamConverters}
-import com.sun.imageio.spi.OutputStreamImageOutputStreamSpi
 
-import scala.io.StdIn
+import akka.http.scaladsl.marshalling.ToResponseMarshaller
+
+import akka.http.scaladsl.server.StandardRoute
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.StreamConverters
+
 import scala.language.implicitConversions
 import de.heikoseeberger.akkahttpcirce.CirceSupport._
 import io.circe.generic.auto._
+
+
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server._
+import StatusCodes._
+import Directives._
 
 object WebServer extends App {
 
@@ -23,7 +26,33 @@ object WebServer extends App {
   // needed for the future flatMap/onComplete in the end
   implicit val executionContext = system.dispatcher
 
+
+  val extenstionExtractor = "(?!\\.)[a-z]+$$".r
+
+
   implicit def htmlUTF8(content: String): StandardRoute = complete(HttpResponse(entity = HttpEntity(ContentTypes.`text/html(UTF-8)`, content)))
+
+  /**
+    * Complete the request by streaming a classpath content.
+    * @param path
+    * @return
+    */
+  def streamFromClasspath(path: String): StandardRoute = {
+    val entity = for {
+      ext <- extenstionExtractor.findFirstIn(path)
+      contentType <- ext match {
+        case "html" => Some(MediaTypes.`text/html`.withCharset(HttpCharsets.`UTF-8`))
+        case "png" => Some(MediaTypes.`image/png`.toContentType)
+        case _ => None
+      }
+      inputStream <- Option(getClass.getResourceAsStream(path))
+    } yield HttpEntity(contentType = contentType, StreamConverters.fromInputStream(() => inputStream))
+
+    entity.map(complete(_))
+      .getOrElse(
+        complete(NotFound, "Nope"))
+  }
+
 
   val lottery = system.actorOf(Props[Lottery], "lottery")
 
@@ -31,7 +60,7 @@ object WebServer extends App {
   val route =
     path("") {
       get {
-        complete(HttpEntity(contentType = MediaTypes.`text/html`.withCharset(HttpCharsets.`UTF-8`), StreamConverters.fromInputStream(() => getClass.getResourceAsStream(s"/public/index.html"))))
+        streamFromClasspath("/public/index.html")
       }
     } ~
       path("winners") {
@@ -47,7 +76,7 @@ object WebServer extends App {
       } ~ path("diagram" / Remaining) {
       fileName =>
         get {
-          complete(HttpEntity(contentType = MediaTypes.`image/png`, StreamConverters.fromInputStream(() => getClass.getResourceAsStream(s"/diagram/$fileName"))))
+          streamFromClasspath(s"/diagram/$fileName")
         }
     }
 
